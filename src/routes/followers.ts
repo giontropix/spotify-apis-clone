0f2ed12630100a8e5fcd63c67803a3e475bc7b83
giontropix <giontropix@msn.com>
2021-04-08 17:27:04 +0200

    + added pagination and filtering to api get all user's followed

--- src/routes/followers.ts
+++ src/routes/followers.ts
@@ -9,13 +9,13 @@ import {Follower} from "../models/Follower";
 const router = express.Router({mergeParams: true});
 
 //AGGIUNGERE USER TRA I FOLLOWED
-router.put("/followed", body('userIdToFollow').isString().notEmpty(), handleErrors, ({body: {userIdToFollow}, params:{id}}, res: Response) => {
+router.put("/followed", body('userIdToFollow').isString().notEmpty(), handleErrors, ({body: {userIdToFollow}, params: {id}}, res: Response) => {
     const currentUser = listOfUsers.find((user: User) => user.id === id)
-    if(!currentUser) return res.status(404).json({error: "User not found"})
+    if (!currentUser) return res.status(404).json({error: "User not found"})
     const userToFollow = listOfUsers.find((user: User) => user.id === userIdToFollow)
-    if(!userToFollow) return res.status(404).json({error: "User to follow not found"})
-    if(userToFollow === currentUser) return res.status(403).json({error: "You can't follow yourself!"})
-    if(currentUser.followed.find((item: Follower) => item.id === userToFollow.id))
+    if (!userToFollow) return res.status(404).json({error: "User to follow not found"})
+    if (userToFollow === currentUser) return res.status(403).json({error: "You can't follow yourself!"})
+    if (currentUser.followed.find((item: Follower) => item.id === userToFollow.id))
         return res.status(403).json({error: "User already followed"})
     currentUser.followed.push(new Follower(userToFollow.id, userToFollow.user_name, userToFollow.sex))
     userToFollow.followers.push(new Follower(currentUser.id, currentUser.user_name, currentUser.sex))
@@ -24,31 +24,38 @@ router.put("/followed", body('userIdToFollow').isString().notEmpty(), handleErro
 })
 
 //VEDERE CHI SEGUE UN UTENTE
-router.get("/followed", ({params: {id}, query: {offset, limit}}: Request, res:Response) => {
+router.get("/followed", ({params: {id}, query: {filter, offset, limit}}: Request, res: Response) => {
     const currentUser = listOfUsers.find((user: User) => user.id === id)
-    if(!currentUser) return res.status(404).json({error: "User not found"})
-    if(offset && limit) return res.status(200).json(currentUser.followed.slice(Number(offset), Number(offset) + Number(limit)))
+    if (!currentUser) return res.status(404).json({error: "User not found"})
+    if(filter){
+        let followedToSearch: Follower | Follower[] | undefined = currentUser.followed.filter((foll: Follower) => {
+            if (foll.user_name.toLowerCase().includes(String(filter).toLowerCase())) return foll
+        });
+        if (offset && limit) return res.status(200).json(followedToSearch.slice(Number(offset), Number(offset) + Number(limit)))
+        return res.status(200).json(followedToSearch)
+    }
+    if (offset && limit) return res.status(200).json(currentUser.followed.slice(Number(offset), Number(offset) + Number(limit)))
     return res.status(200).json(currentUser.followed);
 })
 
 //VEDERE DA CHI E' SEGUITO UN UTENTE
-router.get("/followers", ({params: {id}, query: {offset, limit}}: Request, res:Response) => {
+router.get("/followers", ({params: {id}, query: {offset, limit}}: Request, res: Response) => {
     const currentUser = listOfUsers.find((user: User) => user.id === id)
-    if(!currentUser) return res.status(404).json({error: "User not found"})
-    if(offset && limit) return res.status(200).json(currentUser.followers.slice(Number(offset), Number(offset) + Number(limit)))
+    if (!currentUser) return res.status(404).json({error: "User not found"})
+    if (offset && limit) return res.status(200).json(currentUser.followers.slice(Number(offset), Number(offset) + Number(limit)))
     return res.status(200).json(currentUser.followers);
 })
 
 //SMETTERE DI SEGUIRE UN UTENTE
-router.delete("/followed/:userIdToUnfollow", ({params:{id, userIdToUnfollow}}:Request, res: Response) => {
+router.delete("/followed/:userIdToUnfollow", ({params: {id, userIdToUnfollow}}: Request, res: Response) => {
     const currentUser = listOfUsers.find((user: User) => user.id === id)
-    if(!currentUser) return res.status(404).json({error: "User not found"})
+    if (!currentUser) return res.status(404).json({error: "User not found"})
     const userToUnfollow = listOfUsers.find((user: User) => user.id === userIdToUnfollow)
-    if(!userToUnfollow) return res.status(404).json({error: "User to unfollow not found"})
+    if (!userToUnfollow) return res.status(404).json({error: "User to unfollow not found"})
     const userToUnfollowIndex = currentUser.followed.findIndex((user: Follower) => user.id === userIdToUnfollow)
-    if(userToUnfollowIndex === -1) return res.status(404).json({error: "User to unfollow not found"})
+    if (userToUnfollowIndex === -1) return res.status(404).json({error: "User to unfollow not found"})
     const currentUserIndex = userToUnfollow.followers.findIndex((user: Follower) => user.id === id)
-    if(currentUserIndex === -1) return res.status(404).json({error: "User index not found"})
+    if (currentUserIndex === -1) return res.status(404).json({error: "User index not found"})
     currentUser.followed.splice(userToUnfollowIndex, 1)
     userToUnfollow.followers.splice(currentUserIndex, 1)
     writeToFile()
@@ -56,15 +63,15 @@ router.delete("/followed/:userIdToUnfollow", ({params:{id, userIdToUnfollow}}:Re
 })
 
 //SMETTERE DI FARSI SEGUIRE DA UN UTENTE
-router.delete("/followers/:userIdToUnfollow", ({params:{id, userIdToUnfollow}}:Request, res: Response) => {
+router.delete("/followers/:userIdToUnfollow", ({params: {id, userIdToUnfollow}}: Request, res: Response) => {
     const currentUser = listOfUsers.find((user: User) => user.id === id)
-    if(!currentUser) return res.status(404).json({error: "User not found"})
+    if (!currentUser) return res.status(404).json({error: "User not found"})
     const userToBlock = listOfUsers.find((user: User) => user.id === userIdToUnfollow)
-    if(!userToBlock) return res.status(404).json({error: "User to block not found"})
+    if (!userToBlock) return res.status(404).json({error: "User to block not found"})
     const userToBlockIndex = currentUser.followers.findIndex((user: Follower) => user.id === userIdToUnfollow)
-    if(userToBlockIndex === -1) return res.status(404).json({error: "User to block not found"})
+    if (userToBlockIndex === -1) return res.status(404).json({error: "User to block not found"})
     const currentUserIndex = userToBlock.followed.findIndex((user: Follower) => user.id === id)
-    if(currentUserIndex === -1) return res.status(404).json({error: "User not found"})
+    if (currentUserIndex === -1) return res.status(404).json({error: "User not found"})
     currentUser.followers.splice(userToBlockIndex, 1)
     userToBlock.followed.splice(currentUserIndex, 1)
     writeToFile()
