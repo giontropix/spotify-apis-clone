9628076a644dfac8d7ec0f690cf4d82e99d8da06
giontropix <giontropix@msn.com>
2021-04-08 16:41:22 +0200

    + added pagination and filtering to api get users.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from "express";
-import {listOfUsers, writeToFile} from "../utils/manageUsersFromJSON";
+import {listOfUsers, readFileMiddleware, writeToFile} from "../utils/manageUsersFromJSON";
 import {User} from "../models/User";
 import {playlists} from "./playlists";
 import {followers} from "./followers";
@@ -11,7 +11,18 @@ const router = express.Router({mergeParams: true});
 router.use("/:id/playlists", playlists)
 router.use("/:id", followers)
 
-router.get("/", (_: Request, res: Response) => res.status(200).json(listOfUsers))
+router.get("/", readFileMiddleware, ({query: {filter, offset, limit, option}}: Request, res: Response) => {
+    if (filter) {
+        let userToSearch: User | User[] | undefined = listOfUsers.filter((users: User) => {
+            if (users.user_name.toLowerCase().includes(String(filter).toLowerCase()) ||
+                users.mail.toLowerCase().includes(String(filter).toLowerCase())) return users
+        });
+        if (offset && limit) return res.status(200).json(userToSearch.slice(Number(offset), Number(offset) + Number(limit)))
+        userToSearch && res.status(200).json(userToSearch) || res.status(404).json({message: "Error"})
+    } else if (offset && limit) {
+        res.status(200).json(listOfUsers.slice(Number(offset), Number(offset) + Number(limit)))
+    } else return res.status(200).json(listOfUsers)
+})
 
 router.get("/:id", ({params: {id}}, res: Response) => {
     const user = listOfUsers.find((user: User) => user.id === id)
@@ -20,22 +31,22 @@ router.get("/:id", ({params: {id}}, res: Response) => {
 })
 
 //INCREMENTA ASCOLTI CANZONE
-router.put('/:id', readSongsFileMiddleware, ({params: {id}, body:{song_id}}:Request, res:Response) => {
+router.put('/:id', readSongsFileMiddleware, ({params: {id}, body: {song_id}}: Request, res: Response) => {
     const user = listOfUsers.find((user: User) => user.id === id)
-    if(!user) return res.status(404).json({error: "User not found"})
+    if (!user) return res.status(404).json({error: "User not found"})
     const song = songsList.find(({id}) => id === song_id)
-    if(!song) return res.status(404).json({message: "Song not found!"})
+    if (!song) return res.status(404).json({message: "Song not found!"})
     updateListeningStats(user, song)
     writeSongsToFile()
     writeToFile()
     res.status(201).json({message: "Views increased"})
 })
 
-router.get("/:id/suggestedSongs", readSongsFileMiddleware, ({params: {id}}: Request, res:Response) => {
+router.get("/:id/suggestedSongs", readSongsFileMiddleware, ({params: {id}}: Request, res: Response) => {
     const user = listOfUsers.find((user: User) => user.id === id)
-    if(!user) return res.status(404).json({error: "User not found"})
+    if (!user) return res.status(404).json({error: "User not found"})
     const c = getMostListenedGenres(user).map(genre => songsList.filter((song) => song.genre === genre.genre)).reduce((acc, val) => acc.concat(val), []);
     res.status(200).json([...c]);
 })
 
-export {router as users }
+export {router as users}
